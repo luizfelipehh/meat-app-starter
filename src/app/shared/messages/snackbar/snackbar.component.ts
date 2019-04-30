7e33fc34f1017d984da43012635e7c2ae643865a
Luiz Felipe <lfheemann@hotmail.com>
2019-04-29 23:12:52 -0300

    Adicionado animações para menu-item e shopping-cart

--- src/app/shared/messages/snackbar/snackbar.component.ts
+++ src/app/shared/messages/snackbar/snackbar.component.ts
@@ -39,7 +39,7 @@ export class SnackbarComponent implements OnInit {
       .do(message=>{
         this.message = message
         this.snackVisibility = 'visible'      
-    }).switchMap(message => Observable.timer(1000))
+    }).switchMap(message => Observable.timer(3000))
       .subscribe(timer=> this.snackVisibility = 'hidden')
   }
 
